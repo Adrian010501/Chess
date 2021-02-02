99bacfd7204f5a1cc5969d3d6f60cca2f7198d2d
jacobgeorge3 <jacob.c.george333@gmail.com>
2021-02-02 13:02:53 -0600

    Wrote printBoard()

--- Chess/src/chess/Chess.java
+++ Chess/src/chess/Chess.java
@@ -11,7 +11,14 @@ public class Chess {
 		
 	}
 	public static void printBoard() {
-		//Print board 
+		for(int i = 0; i < 8; i++) {//Traversing vertically(rows)
+			for(int j = 0; j < 8;) {//Traversing horizontally(columns)
+				System.out.print("|" + board[i][j]);//Print data at i , j
+				if(j == 7)
+					System.out.print("|");//Print ending "|" in last column
+			}
+			System.out.print("\n");//New row
+		}
 	}
 	public static void checkMate() {
 		//Check for mate
