3ab92ee4264a2f3cbaffb356806ad23192f57038
jacobgeorge3 <jacob.c.george333@gmail.com>
2021-02-02 12:47:39 -0600

    Commented on first methods we gonna create

--- Chess/src/chess/Chess.java
+++ Chess/src/chess/Chess.java
@@ -1,8 +1,21 @@
 package chess;
 //Jacob is in town
 public class Chess {
-	public static void main(String[] args) {
-		System.out.println("HI");
+	public static void playGame() {
+		//Create board
+		//Starting method
 	}
+	public static void move() {
+		//call getmove
+		//call validate move
+		
+	}
+	public static void printBoard() {
+		//Print board 
+	}
+	public static void checkMate() {
+		//Check for mate
+	}
+	
 
 }
---
+++ Chess/src/chess/Main.java
@@ -0,0 +1,10 @@
+package chess;
+
+public class Main {
+	public static void main(String[] args) {
+
+		while(true) {
+			//Call playgame
+		}
+	}
+}
